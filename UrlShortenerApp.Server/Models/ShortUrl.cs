c2c6f6c8fee9fca4c8608e95b9c5886f7133a71d
serswik <megasergik@icloud.com>
2025-03-19 17:28:42 +0200

    Added shorturl model and models folder

---
+++ UrlShortenerApp.Server/Models/ShortUrl.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace UrlShortenerApp.Server.Models
+{
+    public class ShortUrl
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string OriginalUrl { get; set; } = string.Empty;
+
+        [Required]
+        public string ShortCode { get; set; } = string.Empty;
+
+        public string CreatedBy { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
