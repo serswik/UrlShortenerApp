491a5bf379070dbeb9eb3f3b8747227846fc9663
serswik <megasergik@icloud.com>
2025-03-19 17:32:56 +0200

    Added database context

---
+++ UrlShortenerApp.Server/Context/ApplicationDbContext .cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UrlShortenerApp.Server.Models;
+
+namespace UrlShortenerApp.Server.Context
+{
+    public class ApplicationDbContext  : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ShortUrl> ShortUrls {  get; set; }
+    }
+}
